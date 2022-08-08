e7b75855157af34ea012be53e09ba1ca36361c19
tulioDev <tuliorsc10@gmail.com>
2022-08-08 20:58:00 -0300

    Servidor Funcionando

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cors());
+
+app.get('/', (req,res) => {
+    res.send('Hello,World!')
+})
+
+export default app;
---
+++ src/server.js
@@ -0,0 +1,11 @@
+import app from './app.js';
+
+const porta = process.env.PORT || 3000;
+
+const server = app.listen(porta, () => console.log(`App ouvindo na porta ${porta}`));
+
+// Para o caso de interromper a porta e a api ainda ficar em funcionamento
+process.on('SIGINT', () => {
+  server.close();
+  console.log('App finalizado');
+});
